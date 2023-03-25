9f2f0f82052a6e9c2996605e4ac2cd36e414e782
dlittig <david0605.littig@hotmail.de>
2023-03-25 19:12:47 +0100

    chore: 🔧 Updated app configs to be compatible with EAS

--- app.config.js
+++ app.config.js
@@ -1,8 +1,10 @@
 module.exports = () => {
-  // if (process.env.APP_ENV === "production") {
-  //   return require("./app.production.json");
-  // } else {
-  //   return require("./app.development.json");
-  // }
-  return require("./app.json");
+  if (process.env.APP_ENV === "production") {
+    const config = require("./app.production.json");
+    config.expo.android.config.googleMaps.apiKey = process.env.GOOGLE_API_KEY;
+
+    return config;
+  } else {
+    return require("./app.json");
+  }
 };
